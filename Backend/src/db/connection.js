e2e412309e9b688ed3544ae24ea355dc20e74746
Bhushan_bhatta <bhushanbhatta777@gmail.com>
2025-11-09 13:57:41 +0545

    minor change

--- Backend/src/db/connection.js
+++ Backend/src/db/connection.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`🎉 MongoDB connected! Host: ${connectionInstance.connection.host}`);
+    console.log(`🎉 MongoDB connected successfully! Host: ${connectionInstance.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB connection error:", error);
+    console.error("❌ !! MongoDB connection error:", error);
     process.exit(1);
   }
 };
