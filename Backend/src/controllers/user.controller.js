ff99960f037de408d2bd237d04c0c1cff1d48a71
Bhushan_bhatta <bhushanbhatta777@gmail.com>
2025-11-16 14:01:30 +0545

    email OPT issues fixing

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -31,12 +31,21 @@ const generateAccessAndRefreshTokens = async (userId) => {
 };
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
 });
+
+
 
 const generateOTP = () => crypto.randomInt(100000, 999999).toString();
 
