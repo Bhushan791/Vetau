b3a142c03939a72537744ce0452dd4ddd0b4bf48
Bhushan_bhatta <bhushanbhatta777@gmail.com>
2025-11-30 09:33:29 +0545

    test commit

--- adminpanel/src/App.jsx
+++ adminpanel/src/App.jsx
@@ -1,5 +1,3 @@
-// App.jsx - Main Application Component (NUCLEAR FIX)
-
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
